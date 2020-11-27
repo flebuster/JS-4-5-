52a3e78893ecd6467cbcace06a973a0cb4dcdefc
flebuster <flebuster@tut.by>
2020-11-27 14:00:14 +0300

    second

--- js/sript.js
+++ js/sript.js
@@ -1,8 +1,31 @@
-function firstButton() {
+function firstButton() {                        // первое задание
 const arr1 = [45, 'line', true];
-
 for ( let i = 0; i < arr1.length; i++){
  alert(arr1[i]);
     }
 }
+
+document.querySelector('.secondButton').onclick = function() {     // второе задание
+const arr2 = [44, 'line', false];
+for ( let i = 0; i < arr2.length; i++){
+alert(arr2[i]);
+}
+return (arr2[i]);
+}
+
+function thirdButton() {                                      // третье задание                
+const arr3 = [46, 'line', false];
+for ( let i = 0; i < arr3.length; i++){
+alert(arr3[i]);
+}
+return (arr3[i]);
+}
+document.querySelector('.thirdButton').onclick = thirdButton;
+
+
+
+
+
+
+
 
